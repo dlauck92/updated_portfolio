074fad8ef427a6202b02f2ad3232123c763a9b70
dlauck92 <43586307+dlauck92@users.noreply.github.com>
2019-01-05 07:10:57 -0500

    Add files via upload

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+var config = {
+    apiKey: "AIzaSyB71lDzZTvysoESkozpD29PfFcVt5DoHb8",
+    authDomain: "portfolio-5db72.firebaseapp.com",
+    databaseURL: "https://portfolio-5db72.firebaseio.com",
+    projectId: "portfolio-5db72",
+    storageBucket: "portfolio-5db72.appspot.com",
+    messagingSenderId: "701513185862"
+  };
+  
+  firebase.initializeApp(config);
